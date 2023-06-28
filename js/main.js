a7297737d5c40b320ec1c9d2a1fe6522dcac724d
iteenfk <125781734+iteenfk@users.noreply.github.com>
2023-06-28 15:49:45 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,48 +1,27 @@
-let seconds = 0;
+"use strict"
 
-function convertTime(){
-    let time = document.getElementById("time").value;
-    let unit = document.getElementById("unit").value;
+const volumeSlider = document.getElementById("slider");
 
-    if (unit === "秒"){
-        seconds = time;
-    }else if (unit === "分"){
-        seconds = time * 60;
-    }else if (unit === "時間"){
-        seconds = time * 60 * 60;
-    }else if (unit === "日"){
-        seconds = time * 60 * 60 *24;
-    }else if (unit === "週"){
-        seconds = time * 60 * 60 *24 *7;
-    }else if (unit === "月"){
-        seconds = time * 60 * 60 *24 *7 *30;
-    }else if (unit === "年"){
-        seconds = time * 60 * 60 *24 *7 *30 *365;
-    }
+const volumeValue = document.getElementById("vol");
 
-    const minutes = seconds / 60;
-    const hours = minutes / 60;
-    const days = hours / 24;
-    const weeks = days / 7;
-    const months = days/ 30;
-    const years = days / 365;
+const audio = document.getElementById("player");
 
-    const secondsHtml = Number(seconds).toFixed(2)
-    const minutesHtml = Number(minutes).toFixed(2)
-    const hoursHtml = Number(hours).toFixed(2)
-    const daysHtml = Number(days).toFixed(2)
-    const weeksHtml = Number(weeks).toFixed(2)
-    const monthsHtml = Number(months).toFixed(2)
-    const yearsHtml = Number(years).toFixed(2)
-    const change = document.getElementById("change-box")
-    change.innerHTML = `
-        <p>変換結果を表示</p>
-        <p>${secondsHtml}秒</p>
-        <p>${minutesHtml}分</p>
-        <p>${hoursHtml}時間</p>
-        <p>${daysHtml}日</p>
-        <p>${weeksHtml}週</p>
-        <p>${monthsHtml}月</p>
-        <p>${yearsHtml}年</p>
-    `;
+function volume() {
+    const value = volumeSlider.value;
+    volumeValue.textContent = value + "%"
+    
+    audio.volume = value / 100;
+}
+
+function play(){
+    audio.play();
+}
+
+function pause(){
+    audio.pause();
+}
+
+function stop(){
+    audio.pause();
+    audio.currentTime = 0;
 }
